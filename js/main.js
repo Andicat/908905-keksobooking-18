5ce7a1d54987bf240c78a9702a22ebedd539decd
Katerina <andicat@mail.ru>
2019-09-27 15:44:35 +0300

    show map, get address and vadidate of rooms/capacity

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@
 var OFFERS_NEARBY_AMOUNT = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var PIN_MAIN_WIDTH = 65;
+var PIN_MAIN_HEIGHT = 65;
+var PIN_MAIN_ACTIVE_HEIGHT = 87;
 var PRICE_MIN = 1000;
 var PRICE_MAX = 10000;
 var ROOMS_MIN = 1;
 var ROOMS_MAX = 4;
 var GUESTS_MIN = 1;
 var GUESTS_MAX = 10;
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var RUSSIAN_WORDS = {flat: 'Квартира', bungalo: 'Бунгало', house: 'Дом', palace: 'Дворец'};
@@ -17,12 +22,15 @@ var CHECK_TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
+var mapPinMain = document.querySelector('.map__pin--main');
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPins = document.querySelector('.map__pins');
 var map = document.querySelector('.map');
 var mapFilter = document.querySelector('.map__filters-container');
 var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var adForm = document.querySelector('.ad-form');
+var filterForm = document.querySelector('.map__filters');
+var go = false;
 
 // получаем случайный элемент из массива
 function getRandomElementFromArray(arr) {
@@ -141,20 +149,86 @@ function showCard(offer) {
 }
 
 // делаем элементы формы неактивными
-function disabledForm(form) {
-  var formElements = form.querySelectorAll('fieldset');
-  for (var i = 0; i< formElements.length; i++) {
-    formElements[i].disabled = true;
+function disabledForm(form, disabled) {
+  if (disabled) {
+    form.classList.add('ad-form--disabled');
+  } else {
+    form.classList.remove('ad-form--disabled');
+  }
+  var formElements = form.elements;
+  for (var i = 0; i < formElements.length; i++) {
+    formElements[i].disabled = disabled;
   }
 }
 
-disabledForm(adForm);
-//map.classList.remove('map--faded');
+// активируем карту и форму
+function activateMap() {
+  map.classList.remove('map--faded');
+  disabledForm(adForm, false);
+  disabledForm(filterForm, false);
+}
 
-// создаем массив соседних предложений
-var offers = createOffersArray(OFFERS_NEARBY_AMOUNT);
-// вставляем на карту метки из массива
-//createPins(offers);
+// устанавливаем значение поля адреса
+function setPinMainAddress(pinActive) {
+  var x = mapPinMain.offsetLeft + PIN_MAIN_WIDTH / 2;
+  var y = mapPinMain.offsetTop + (pinActive ? PIN_MAIN_ACTIVE_HEIGHT : PIN_MAIN_HEIGHT / 2);
+  adForm.address.value = Math.round(x) + ', ' + Math.round(y);
+}
 
-// показываем карточку первого предложения по умолчанию
-//showCard(offers[0]);
+// активируем главную метку
+function activatePinMain() {
+  activateMap();
+  setPinMainAddress(true);
+}
+
+// проверка соответствия кол-ва комнат и гостей
+function checkRoomsCapacity() {
+  var roomsCapacity = adForm.rooms.value;
+  var capacityOptions = adForm.capacity.options;
+  for (var i = 0; i < capacityOptions.length; i++) {
+    if (roomsCapacity < 100) {
+      capacityOptions[i].disabled = ((capacityOptions[i].value > 0 & capacityOptions[i].value <= roomsCapacity) ? false : true);
+    } else {
+      capacityOptions[i].disabled = (capacityOptions[i].value > 0 ? true : false);
+    }
+  }
+  if (capacityOptions[capacityOptions.selectedIndex].disabled) {
+    capacityOptions[capacityOptions.selectedIndex].selected = false;
+  }
+}
+
+// клик мышкой на главную метку
+mapPinMain.addEventListener('mousedown', activatePinMain);
+adForm.rooms.addEventListener('change', checkRoomsCapacity);
+
+// обработка нажатия клавиш на клавиатуре
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    evt.preventDefault();
+    if (evt.target === mapPinMain) {
+      activatePinMain();
+    }
+  }
+
+  if (evt.keyCode === ESC_KEYCODE) {
+    evt.preventDefault();
+  }
+});
+
+disabledForm(adForm, true);
+disabledForm(filterForm, true);
+
+// подставляются координаты центра метки
+setPinMainAddress(false);
+checkRoomsCapacity();
+
+if (go) {
+  // создаем массив соседних предложений
+  var offers = createOffersArray(OFFERS_NEARBY_AMOUNT);
+
+  // вставляем на карту метки из массива
+  createPins(offers);
+
+  // показываем карточку первого предложения по умолчанию
+  showCard(offers[0]);
+}
