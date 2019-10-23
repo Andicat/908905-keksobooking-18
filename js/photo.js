51796cfb82c8aa050314dd82d216bb6e1492f827
Katerina <andicat@mail.ru>
2019-10-23 10:47:00 +0300

    load multiple photos

--- js/photo.js
+++ js/photo.js
@@ -6,35 +6,59 @@
   var avatarFileChooser = document.querySelector('.ad-form-header__upload input[type=file]');
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
   var photoFileChooser = document.querySelector('.ad-form__upload input[type=file]');
-  var photoPreview = document.querySelector('.ad-form__photo');
+  var photoContainer = document.querySelector('.ad-form__photo-container');
+  var photoPreviewTemplate = document.querySelector('.ad-form__photo');
 
-  function loadPhoto(evt) {
-    var fileChooser = evt.target;
-    var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
+  function loadImage(file, target) {
+    var reader = new FileReader(file);
 
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
+    reader.addEventListener('load', function () {
+      if (target === avatarFileChooser) {
+        avatarPreview.src = reader.result;
+      }
+      if (target === photoFileChooser) {
+        var photoPreview = photoPreviewTemplate.cloneNode(true);
+
+        photoPreview.style.backgroundImage = 'url(' + reader.result + ')';
+        photoPreview.style.backgroundRepeat = 'no-repeat';
+        photoPreview.style.backgroundSize = 'cover';
+
+        photoContainer.appendChild(photoPreview);
+      }
     });
 
-    if (matches) {
-      var reader = new FileReader();
+    reader.readAsDataURL(file);
+  }
 
-      reader.addEventListener('load', function () {
-        if (evt.target === avatarFileChooser) {
-          avatarPreview.src = reader.result;
-        }
-        if (evt.target === photoFileChooser) {
-          photoPreview.style.backgroundImage = 'url(' + reader.result + ')';
-          photoPreview.style.backgroundRepeat = 'no-repeat';
-          photoPreview.style.backgroundSize = 'cover';
-        }
+  function deletePhotos(target) {
+    if (target === photoFileChooser) {
+      var photos = photoContainer.querySelectorAll('.ad-form__photo');
+
+      photos.forEach(function (it) {
+        photoContainer.removeChild(it);
       });
-
-      reader.readAsDataURL(file);
     }
   }
 
-  avatarFileChooser.addEventListener('change', loadPhoto);
-  photoFileChooser.addEventListener('change', loadPhoto);
+  function loadPhotos(evt) {
+    var fileChooser = evt.target;
+    var files = fileChooser.files;
+
+    for (var i = 0; i < files.length; i++) {
+      var fileName = files[i].name.toLowerCase();
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+
+      if (matches) {
+        deletePhotos(evt.target);
+        loadImage(files[i], evt.target);
+      }
+    }
+  }
+
+  photoFileChooser.setAttribute('multiple', 'multiple');
+
+  avatarFileChooser.addEventListener('change', loadPhotos);
+  photoFileChooser.addEventListener('change', loadPhotos);
 })();
